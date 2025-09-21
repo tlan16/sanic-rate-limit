f516889f48e63a40729d1698c3b2743ab868b3c7
Frank Lan <franklan118@gmail.com>
2025-09-21 18:20:18 +1000

    multiple workers

---
+++ config.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+from pydantic import BaseModel, Field
+import os
+from dotenv import load_dotenv
+
+class EnvironmentVariables(BaseModel):
+    APP_DEBUG: bool | None = None
+    APP_WORKERS: int | None = Field(default=None, gt=0)
+
+@lru_cache(maxsize=1)
+def get_config() -> EnvironmentVariables:
+    load_dotenv()
+    return EnvironmentVariables.model_validate(os.environ)
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+import os
+from functools import lru_cache
+
 from sanic import Sanic
 from sanic.response import text
 
+from config import get_config
+
 app = Sanic("rate_limit_demo")
+config = get_config()
 
 @app.get("/")
 async def hello_world(_request):
     return text("OK")
 
+@lru_cache(maxsize=1)
+def get_number_of_workers() -> int:
+    if config.APP_WORKERS is not None:
+        if config.APP_DEBUG:
+            print(f"Using {config.APP_WORKERS} worker(s) from env var APP_WORKERS")
+        return config.APP_WORKERS
+
+    number_of_cpus = os.cpu_count() or 1
+    number_of_cpus = max(1, number_of_cpus)
+    if config.APP_DEBUG:
+        print(f"Using {number_of_cpus} worker(s) based on CPU count")
+    return number_of_cpus
+
 
 def main():
-    app.run()
+    app.run(
+        workers=get_number_of_workers()
+    )
 
 
 if __name__ == "__main__":
